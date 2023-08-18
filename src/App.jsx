4e7eff6b25424c21b25401f58fe42086ec5f9870
Soraya Artiel <artielsoraya@gmail.com>
2023-08-18 18:51:35 +0200

    Styles added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 	
 	return (
 		<>
-		<h1>Infinite Scrolling with IntersectionObserver API</h1>
 			<ProductList/>
 		</>
 	);
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import style from './productList.module.css';
 
 const ProductList = () => {
 	const [products, setProducts] = useState([]);
@@ -26,16 +27,15 @@ const ProductList = () => {
 		};
 	}, [products]);
 
-
-
 	async function fetchMoreItems() {
 		// fetch los proximos productos
 		const response = await fetch(
 			`https://dummyjson.com/products?limit=10&skip=${page * 10}`
 		);
 		const data = await response.json();
+		console.log(data);
 
-		if (data.products.length < 0) {
+		if (data.products.length === 0) {
 			setHasMore(false);
 		} else {
 			setProducts(prevProducts => [...prevProducts, ...data.products]);
@@ -44,12 +44,28 @@ const ProductList = () => {
 	}
 
 	return (
-		<ul>
-			{products.map(item => (
-				<li key={item.id}>{item.description}</li>
-			))}
-			{hasMore && <div ref={elementRef}>Loading more items...</div>}
-		</ul>
+		<>
+			<h1 className={style.title}>
+				Infinite Scrolling with IntersectionObserver API and ReactJS
+			</h1>
+			<div className={style.container}>
+				{products.map(item => (
+					<ul className={style.ul} key={item.id}>
+						<li className={style.li}>
+							<p className={style.name}>{item.title}</p>
+							<p className={style.description}>{item.description}</p>
+							<p className={style.brand}>{item.brand}</p>
+						</li>
+						<img className={style.img} src={item.images[0]} alt={item.title} />
+					</ul>
+				))}
+			</div>
+			{hasMore && (
+				<div className={style.loading} ref={elementRef}>
+					Loading items...
+				</div>
+			)}
+		</>
 	);
 };
 
